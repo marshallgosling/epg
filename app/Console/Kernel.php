6336c4b1aa2d76b29c62971a759d4d964a9f448c
root <migo@local>
2023-10-20 17:04:29 +0800

    daily schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('tools:crawler',["url"=>'daily'])->twiceDailyAt(6, 18);
+        $schedule->command('tools:crawler',['daily'])->twiceDailyAt(6, 18);
     }
 
     /**
