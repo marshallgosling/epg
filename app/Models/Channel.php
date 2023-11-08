223cca3d3fbe88c6a3d9537a81e62e84bd676338
root <migo@local>
2023-11-08 08:29:41 +0800

    channel status change

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -9,11 +9,12 @@ class Channel extends Model
 {
     use HasFactory;
 
-    public const STATUS_NORMAL = 0;
+    public const STATUS_EMPTY = 0;
     public const STATUS_RUNNING = 1;
-    public const STATUS_CLOSE = 2;
+    public const STATUS_READY = 2;
+    public const STATUS_CLOSE = 3;
 
-    public const STATUS = ['正常', '运行中', '下线'];
+    public const STATUS = ['未编单', '运行中', '正常', '下线'];
     public const AUDIT = ['未审核', '通过', '不通过'];
 
     protected $table = 'channel';
