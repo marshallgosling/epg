e551536033812d8d78b2422799c1727683a9e172
root <migo@local>
2023-11-17 10:01:52 +0800

    fix channel

--- app/Tools/ChannelFixer.php
+++ app/Tools/ChannelFixer.php
@@ -14,16 +14,18 @@ class ChannelFixer
         }
 
         $programs = $channel->programs()->get();
-        $last = strtotime($channel->air_date);
+        $last = strtotime($channel->air_date.' 00:00:00');
+        echo "channel air date is {$channel->air_date}".PHP_EOL;
 
         foreach($programs as $p)
         {
-            $air = strtotime($channel->air_date.' '.$p->start_at);
+            echo "Program {$p->name} air date is {$p->start_at}".PHP_EOL;
+            $air = strtotime($p->start_at);
 
             if($air < $last) {
                 $air += 24 * 3600;
                 
-                echo "fix program: {$p->start_at} {$p->name} to ".date('Y-m-d H:i:s', $air).PHP_EOL;
+                echo "fix program: {$p->start_at} to ".date('Y-m-d H:i:s', $air).PHP_EOL;
 
                 //$p->start_at = date('Y-m-d H:i:s', $air);
                 //$p->save();
