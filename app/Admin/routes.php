6872e58091a6a381babf95834587f834e6c23310
root <migo@local>
2023-12-01 22:53:43 +0800

    create link with querystring

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -84,14 +84,14 @@ Route::group([
         $q = $request->get('q');
             
         return Program::where('name', 'like', "%$q%")->orWhere('unique_no', 'like', "$q%")->orWhere('artist', 'like', "%$q%")
-            ->select(DB::raw('id, concat(unique_no, " ", name, " ", artist) as text, unique_no, duration, name, category, artist, black'))
+            ->select(DB::raw('unique_no as id, concat(unique_no, " ", name, " ", artist) as text'))
             ->paginate(15);
     });
     $router->get('/api/category', function (Request $request) {
         $q = $request->get('q');
     
         return Category::where('no', 'like', "$q%")->where('type', 'tags')
-            ->select(DB::raw('id, concat("【 ",no, " 】 ", name) as text, no as category,  name, duration'))
+            ->select(DB::raw('no as id, concat("【 ",no, " 】 ", name) as text'))
             ->paginate(15);
     });
 });
