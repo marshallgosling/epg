d8ea03b6ca7a18f9c8c76ceb43260beb3d355796
root <migo@local>
2023-12-03 08:57:15 +0800

    upload bug

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,7 +45,7 @@ return [
         'admin' => [
             'driver' => 'local',
             'root' => storage_path('app/public/admin'),
-            'url' => env('APP_URL'),
+            'url' => env('APP_URL').'/storage/admin',
             'visibility' => 'public',
         ],
 
