c44d5b388e3beb93463d6d3f827217fd277bd380
root <migo@local>
2023-12-03 19:46:02 +0800

    play epg

---
+++ app/Admin/Controllers/ChannelXmlController.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Channel;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Illuminate\Support\MessageBag;
+
+class ChannelXmlController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = "节目单 Xml 文件列表";
+
+    protected $description = [
+                'index'  => "查看和生成节目单 EPG 播出数据",
+        //        'show'   => 'Show',
+        //        'edit'   => 'Edit',
+        //        'create' => 'Create',
+    ];
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Channel());
+
+        $grid->model()->orderBy('air_date', 'desc');
+
+        $grid->column('id', __('ID'));
+        $grid->column('name', __('Group'));
+        $grid->column('uuid', __('Uuid'))->display(function($uuid) {
+            return '<a href="channelv/programs?channel_id='.$this->id.'">'.$uuid.'</a>';
+        });
+        $grid->column('air_date', __('Air date'));
+        //$grid->column('name', __('Name'));
+        $grid->column('status', __('Status'))->using(Channel::STATUS)->label(['warning','danger','success','danger']);
+        //$grid->column('comment', __('Comment'));
+        $grid->column('version', __('Version'))->label('default');
+        $grid->column('reviewer', __('Reviewer'));
+        $grid->column('audit_status', __('Audit status'))->using(Channel::AUDIT)->label(['info','success','danger']);;
+        /*$grid->column('audit_date', __('Audit date'));
+        $grid->column('distribution_date', __('Distribution date'));
+        $grid->column('created_at', __('Created at'));
+        */
+        $grid->column('download', __('Download'))->display(function() {
+            return Storage::disk('public')->exists($this->name.'_'.$this->air_date.'.xml') ? 
+                '<a href="'.Storage::disk('public')->url($this->name.'_'.$this->air_date.'.xml').'" target="_blank">下载</a>':'';
+        });
+
+        $grid->column('updated_at', __('Updated at'))->hide();
+
+        $grid->actions(function (Grid\Displayers\Actions $actions) {
+            // $actions->add(new Generator);
+            // $actions->add(new Clean);
+            $actions->disableAll();
+        });
+
+        $grid->filter(function($filter){
+
+            $filter->equal('uuid', __('Uuid'));
+            $filter->date('air_date', __('Air date'));
+            
+        });
+
+        $grid->tools(function (Grid\Tools $tools) {
+            //$tools->append(new ToolExporter());
+        });
+
+        $grid->disableCreateButton();
+        $grid->disableBatchActions();
+        
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Channel::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('uuid', __('Uuid'));
+        $show->field('air_date', __('Air date'));
+        $show->field('name', __('Name'));
+        $show->field('status', __('Status'))->using(Channel::STATUS);
+        $show->field('comment', __('Comment'));
+        $show->field('version', __('Version'));
+        $show->field('reviewer', __('Reviewer'));
+        $show->field('audit_status', __('Audit status'))->using(Channel::AUDIT);
+        $show->field('audit_date', __('Audit date'));
+        $show->field('distribution_date', __('Distribution date'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Channel());
+
+        $form->hidden('name', __('Name'))->default('channelv');
+        $form->text('uuid', __('Uuid'))->default((string) Str::uuid())->required();
+        $form->date('air_date', __('Air date'))->required();      
+        $form->radio('status', __('Status'))->options(Channel::STATUS)->required();
+        $form->text('version', __('Version'))->default('1')->required();
+
+        $form->divider(__('AuditInfo'));
+        $form->text('reviewer', __('Reviewer'));
+        $form->radio('audit_status', __('Audit status'))->options(Channel::AUDIT)->required();
+        $form->date('audit_date', __('Audit date'));
+        $form->text('comment', __('Comment'));
+
+        $form->date('distribution_date', __('Distribution date'));
+
+        $form->saving(function(Form $form) {
+
+            if($form->isCreating()) {
+                $error = new MessageBag([
+                    'title'   => '创建节目单失败',
+                    'message' => '该日期 '. $form->air_date.' 节目单已存在。',
+                ]);
+
+                //$form->name = 'channelv';
+    
+                if(Channel::where('air_date', $form->air_date)->exists())
+                {
+                    return back()->with(compact('error'));
+                }
+            }
+
+            if($form->isEditing()) {
+                $error = new MessageBag([
+                    'title'   => '修改节目单失败',
+                    'message' => '该日期 '. $form->air_date.' 节目单已存在。',
+                ]);
+    
+                if(Channel::where('air_date', $form->air_date)->where('id','<>',$form->model()->id)->exists())
+                {
+                    return back()->with(compact('error'));
+                }
+            }
+
+            //return $form;
+            
+        });
+
+        return $form;
+    }
+}
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -55,6 +55,8 @@ Route::group([
 
     $router->get('/export/download/{id}', 'ExportListController@download')->name('export.download');
     $router->resource('/export/list', 'ExportListController')->names('export.list');
+
+    $router->resource('/export/xml', 'ChannelXmlController')->names('export.xml');
     
     $router->resource('/statistic/list', 'StatisticController')->names('statistic.list');
 
