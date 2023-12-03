43ed09306f8a2e166110627d424612a844eafc0c
root <migo@local>
2023-12-03 16:59:28 +0800

    save audit reviewer and date

--- app/Admin/Actions/Channel/BatchAudit.php
+++ app/Admin/Actions/Channel/BatchAudit.php
@@ -6,6 +6,7 @@ use App\Models\Channel;
 use Encore\Admin\Actions\BatchAction;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Encore\Admin\Facades\Admin;
 
 class BatchAudit extends BatchAction
 {
@@ -25,6 +26,8 @@ class BatchAudit extends BatchAction
             }
             $model->audit_status = $audit;
             $model->comment = $comment;
+            $model->reviewer = Admin::user()->name;
+            $model->audit_date = now();
             $model->save();
             // Channel::where('id', $model->id)->update(['audit_status', $request->get('audit'), 'comment'=>$request->get('comment')]);
 
