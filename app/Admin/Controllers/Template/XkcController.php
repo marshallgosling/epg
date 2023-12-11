aa98a8eeb655f7c7ff0ccb2b710805026ee02677
root <migo@local>
2023-12-11 13:25:43 +0800

    admin ui

--- app/Admin/Controllers/Template/XkcController.php
+++ app/Admin/Controllers/Template/XkcController.php
@@ -38,22 +38,21 @@ class XkcController extends AdminController
         $grid->column('name', __('Name'))->display(function($name) {
             return '<a href="xkc/programs?template_id='.$this->id.'">'.$name.'</a>'; 
         });
-        // $grid->column('ex', __(" "))->display(function() {
-        //     return "预览";
-        // })->expand(function ($model) {
-        //     $programs = $model->programs()->take(10)->get()->map(function ($program) {  
-        //         return $program->only(['id', 'name', 'category', 'order_no', 'created_at']);
-        //     });
+        $grid->column('ex', __(" "))->display(function() {
+            return "编排";
+        })->expand(function ($model) {
+            $programs = $model->programs()->take(10)->get()->map(function ($program) {  
+                return $program->only(['id', 'category', 'type', 'name', 'data']);
+            });
 
-        //     $items = $programs->toArray();
+            $items = $programs->toArray();
             
-        //     if(count($items) == 0) $info = "没有模版条目记录，请点击添加";
-        //     else $info = '当前最多只显示10条记录，请点击查看';
+            if(count($items) == 0) $info = "没有模版条目记录，请点击添加";
+            else $info = '当前最多只显示10条记录，请点击查看';
 
-        //     $infoBox = '<div class="small-box bg-aqua" style="margin-bottom:0"><a href="channelv/tree/'.$this->id.'" class="small-box-footer">'.$info.'<i class="fa fa-arrow-circle-right"></i></a></div>';
             
-        //     return $infoBox.(new Table(['ID', '名称', '栏目', '排序', '创建时间'], $items))->render();
-        // });
+            return new Table(['ID', '栏目', '类型', '别名', '数据'], $items);
+        });
         $grid->column('version', __('Version'))->display(function ($version) {
             return '<span class="label label-default">'.$version.'</span>';
         });
