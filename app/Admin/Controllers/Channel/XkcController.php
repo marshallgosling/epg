12096fdda4e73a28a1a4448c4e9d3feb2ee176a8
root <migo@local>
2023-12-19 11:44:49 +0800

    preview epg

--- app/Admin/Controllers/Channel/XkcController.php
+++ app/Admin/Controllers/Channel/XkcController.php
@@ -47,7 +47,7 @@ class XkcController extends AdminController
             return '<a href="xkc/programs?channel_id='.$this->id.'">'.$uuid.'</a>';
         });
         $grid->column('air_date', __('Air date'))->display(function($air_date) {
-            return '<a href="'.admin_url('epg/preview/'.$this->id).'">'.$air_date.'</a>';
+            return '<a href="'.admin_url('epg/preview/'.$this->id).'" title="预览EPG" data-toggle="tooltip" data-placement="top">'.$air_date.'</a>';
         });
 
         $grid->column('start_end', __('StartEnd'));
