3305b2177d53ab550264e0c1050d7cc6f92e936c
root <migo@local>
2023-12-19 19:20:53 +0800

    cartoon ep feature

--- app/Admin/Controllers/Template/XkcProgramsController.php
+++ app/Admin/Controllers/Template/XkcProgramsController.php
@@ -147,7 +147,7 @@ class XkcProgramsController extends AdminController
             $form->select('episodes', __('Episodes'))->options('/admin/api/episodes');
             $form->dateRange('date_from', 'date_to', __('DateRange'));
             $form->checkbox('dayofweek', __('Daysofweek'))->options(TemplateRecords::DAYS)->canCheckAll();
-            $form->number('ep', __('Ep no'))->min(1)->default(1);
+            $form->number('ep', __('Ep no'))->min(1)->max(4)->default(1);
 
             $form->text("name", __('Name'));
             $form->text('unique_no', __('Unique no'));
