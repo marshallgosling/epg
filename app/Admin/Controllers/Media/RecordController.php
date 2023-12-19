7717ec7af800d45bf72b22f8254eeaf1d2c5fed8
root <migo@local>
2023-12-19 21:50:05 +0800

    ui

--- app/Admin/Controllers/Media/RecordController.php
+++ app/Admin/Controllers/Media/RecordController.php
@@ -58,7 +58,7 @@ class RecordController extends AdminController
         $grid->column('expired_date', __('Expired date'))->hide();
 
         $grid->column('created_at', __('Created at'))->hide()->sortable();
-        $grid->column('updated_at', __('Updated at'))->hide()->sortable();
+        $grid->column('updated_at', __('Updated at'))->sortable();
 
         $grid->filter(function(Grid\Filter $filter){
 
