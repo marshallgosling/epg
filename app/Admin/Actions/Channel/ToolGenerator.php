51a0d51fcfa56b30833ac899ebc647a8808bc91d
root <migo@local>
2023-12-21 10:43:30 +0800

    generator action bug : group value

--- app/Admin/Actions/Channel/ToolGenerator.php
+++ app/Admin/Actions/Channel/ToolGenerator.php
@@ -24,24 +24,26 @@ class ToolGenerator extends Action
 
     public function handle(Request $request)
     {
-        if($this->group == 'xkc' && Storage::disk('data')->exists(XkcGenerator::STALL_FILE))
+        $group = $request->get('group');
+
+        if($group == 'xkc' && Storage::disk('data')->exists(XkcGenerator::STALL_FILE))
         {
             return $this->response()->error('您有未处理的节目单模版数据错误，请先进入临时模版页面，解决模版问题，然后点击解决问题。');
         }
 
-        if(Channel::where(['status'=>Channel::STATUS_ERROR,'name'=>$this->group])->exists())
+        if(Channel::where(['status'=>Channel::STATUS_ERROR,'name'=>$group])->exists())
         {
             return $this->response()->error('节目单有状态为“错误”的情况，请先处理错误的节目单后才能继续。');
         }
 
-        $last = Channel::where(['status'=>Channel::STATUS_EMPTY,'name'=>$this->group])->orderBy('air_date', 'desc')->first();
+        $last = Channel::where(['status'=>Channel::STATUS_EMPTY,'name'=>$group])->orderBy('air_date', 'desc')->first();
         if(!$last) {
             return $this->response()->error('没有节目单需要生成');
         }
-        
+
         $start_at = $request->get('start_at');
         $end_at = $request->get('end_at') ?? $last->air_date;
-        $group = $request->get('group');
+        
         $s = strtotime($start_at);
         $e = strtotime($end_at);
 
@@ -49,7 +51,7 @@ class ToolGenerator extends Action
             return $this->response()->error('结束日期不能早于开始日期');
         }
 
-        $channels = Channel::where(['status'=>Channel::STATUS_EMPTY,'name'=>$this->group])
+        $channels = Channel::where(['status'=>Channel::STATUS_EMPTY,'name'=>$group])
                     ->where('air_date','>=',$start_at)->where('air_date','<=',$end_at)->get();
         
         if($channels) {
