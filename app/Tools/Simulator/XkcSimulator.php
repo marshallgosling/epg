7d99013f67d136f91e1572e2b7c9c6dc74c1aaaa
root <migo@local>
2023-12-22 10:05:44 +0800

    simulator

--- app/Tools/Simulator/XkcSimulator.php
+++ app/Tools/Simulator/XkcSimulator.php
@@ -79,13 +79,14 @@ class XkcSimulator
                 $templateresult = $template->toArray();
 
                 $templateresult['error'] = false;
-                $templateresult['template'] = $template_item;
+                
 
                 if(!$template_item) {
                     $this->info("没有找到匹配的模版数据: {$template->id} {$template->category}");
                     $templateresult['error'] = "没有找到匹配的模版数据: {$template->id} {$template->category}";
                     
                     $templateresult['program'] = $program->toArray();
+                    $templateresult['template'] = json_decode(json_encode($template_item));
 
                     $result['data'][] = $templateresult;
                     $result['error'] = true;
@@ -139,7 +140,7 @@ class XkcSimulator
                 $program->data = $epglist;
                 $program->end_at = date('Y-m-d H:i:s', $air);
                 
-                $templateresult['template'] = $template_item;
+                $templateresult['template'] = json_decode(json_encode($template_item));
                 $templateresult['program'] = $program->toArray();
 
                 $result['data'][] = $templateresult;
