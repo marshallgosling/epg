aae0a70f785a0801a5a48cd2ca74998889d01f1f
root <migo@local>
2023-12-22 21:04:40 +0800

    template preview

--- app/Admin/Controllers/Template/XkcController.php
+++ app/Admin/Controllers/Template/XkcController.php
@@ -46,18 +46,20 @@ class XkcController extends AdminController
             $programs = $t['records'];
             if($programs)foreach($programs as $p)
             {
+                $style = '';
                 if($p['data'] != null) {
                     $days = [];
                     if(count($p['data']['dayofweek']) == 7) $days[] = __('全天');
                     else if($p['data']['dayofweek'])
                         foreach($p['data']['dayofweek'] as $d) $days[] = __(TemplateRecords::DAYS[$d]);
-                    $items[] = [ $p['id'], $p['name'], $p['category'], TemplateRecords::TYPES[$p['type']], $p['data']['episodes'], $p['data']['date_from'].'/'.$p['data']['date_to'], implode(',', $days), $p['data']['name'], $p['data']['result'], '<a href="programs/'.$p['id'].'/edit">查看</a>'];
-                
+                    $item = [ $p['id'], $p['name'], $p['category'], TemplateRecords::TYPES[$p['type']], $p['data']['episodes'], $p['data']['date_from'].'/'.$p['data']['date_to'], implode(',', $days), $p['data']['name'], $p['data']['result'], '<a href="programs/'.$p['id'].'/edit">查看</a>'];
+                    if($p['data']['result'] == '编排完' || $p['data']['result'] == '错误') $style = 'bg-danger';
                 }
                 else {
-                    $items[] = [ $p['id'], $p['name'], $p['category'], TemplateRecords::TYPES[$p['type']], '', '', '', '', '', '<a href="programs/'.$p->id.'/edit">查看</a>' ];
+                    $item = [ $p['id'], $p['name'], $p['category'], TemplateRecords::TYPES[$p['type']], '', '', '', '', '', '<a href="programs/'.$p->id.'/edit">查看</a>' ];
                 
                 }
+                $items[] = compact('style', 'item');
             }
 
             if($t['schedule'] == Template::SPECIAL) $temp['color'] = 'default';
@@ -71,7 +73,7 @@ class XkcController extends AdminController
 
             }
 
-            $temp['table'] = (new Table(['ID', '别名', '栏目', '类型', '剧集', '日期范围', '播出日', '当前选集', '状态', '操作'], $items, ['table-hover']))->render();
+            $temp['table'] = (new MyTable(['ID', '别名', '栏目', '类型', '剧集', '日期范围', '播出日', '当前选集', '状态', '操作'], $items, ['table-hover']))->render();
             $data[] = $temp; 
         
         }
