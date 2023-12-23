83a350b261302de53a15aaf89d96fc5b5099ce6a
root <migo@local>
2023-12-23 10:33:25 +0800

    simulator checker

--- app/Admin/Actions/Channel/ToolGenerator.php
+++ app/Admin/Actions/Channel/ToolGenerator.php
@@ -28,7 +28,7 @@ class ToolGenerator extends Action
 
         if($group == 'xkc' && Storage::disk('data')->exists(XkcGenerator::STALL_FILE))
         {
-            return $this->response()->error('您有未处理的节目单模版数据错误，请先进入临时模版页面，解决模版问题，然后点击解决问题。');
+            return $this->response()->error('您有未处理的模版编排错误，请先进入模版页面，解决模版问题，然后点击“模拟编单测试”按钮。');
         }
 
         if(Channel::where(['status'=>Channel::STATUS_ERROR,'name'=>$group])->exists())
