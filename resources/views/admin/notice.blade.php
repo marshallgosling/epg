856764b265afa394004de2dd25a5b83fb0d6e569
goslin <goslin@gmail.com>
2024-01-05 16:20:19 +0800

    ui

--- resources/views/admin/notice.blade.php
+++ resources/views/admin/notice.blade.php
@@ -1,3 +1,3 @@
-<li><a href="/admin/channel/xkc">星空中国 编单最新日期 <span class="text-danger">{{$xkc}}</span></a></li>
-<li><a href="/admin/channel/xki">星空国际 编单最新日期 <span class="text-danger">{{$xki}}</span></a></li>
-<li><a href="/admin/channel/xkv">V China 编单最新日期 <span class="text-danger">{{$xkv}}</span></a></li>
+<li><a href="/admin/channel/xkc">星空中国 编单日期 <span class="text-danger">{{$xkc}}</span></a></li>
+<li><a href="/admin/channel/xki">星空国际 编单日期 <span class="text-danger">{{$xki}}</span></a></li>
+<li><a href="/admin/channel/xkv">V China 编单日期 <span class="text-danger">{{$xkv}}</span></a></li>
