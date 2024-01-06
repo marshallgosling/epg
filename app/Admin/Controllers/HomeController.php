844c450de3a4f69ec575d2bf7307d41501f7b512
goslin <goslin@gmail.com>
2024-01-06 10:52:02 +0800

    fix some dashboard issue

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -20,7 +20,8 @@ class HomeController extends Controller
         return $content
             ->title('欢迎使用编单及节目管理系统')
             ->description('查看、管理频道编单及节目内容和素材管理')
-            ->row(HomeController::title())
+            ->row(HomeController::links())
+            ->row(HomeController::statistics())
             ->row(function (Row $row) {
 
                 $row->column(8, function (Column $column) {
@@ -83,12 +84,43 @@ HTML;
         return $box->render();
     }
 
-    public static function channels()
+    public static function statistics()
     {
-        return '';
+      $templates = Statistic::countTemplate();
+      $channels = Statistic::countChannelXml();
+      $audit = Statistic::countAudit();
+      
+      foreach(['xkv','xkc','xki'] as $k) {
+        if(!array_key_exists($k, $channels)) $channels[$k] = 0;
+        if(!array_key_exists($k, $audit)) $audit[$k] = 0;
+        if(!array_key_exists($k, $templates)) $templates[$k] = 0;
+      }
+
+      $channels =[
+        '频道 V China' => [
+          '节目库内容数量 <span class="badge">'.Statistic::countPrograms().'</span>' => admin_url('/media/programs'),
+          '模版库数量 <span class="badge">'.$templates['xkv'].'</span>' => admin_url('/template/xkv'),
+          '编单数量 <span class="badge">'.$channels['xkv'].'</span>' => admin_url('/channel/xkv'),
+          '已审核编单 <span class="badge">'.$audit['xkv'].'</span>' => admin_url('/channel/xkv'),
+        ],
+        '频道 星空中国' => [
+          '节目库内容数量 <span class="badge">'.Statistic::countRecords().'</span>' => admin_url('/media/records'),
+          '模版库数量 <span class="badge">'.$templates['xkc'].'</span>' => admin_url('/template/xkc'),
+          '编单数量 <span class="badge">'.$channels['xkc'].'</span>' => admin_url('/channel/xkc'),
+          '已审核编单 <span class="badge">'.$audit['xkc'].'</span>' => admin_url('/channel/xkc'),
+        ],
+        '频道 星空国际' => [
+          '节目库内容数量 <span class="badge">'.Statistic::countRecord2().'</span>' => admin_url('/media/record2'),
+          '模版库数量 <span class="badge">'.$templates['xki'].'</span>' => admin_url('/template/xki'),
+          '编单数量 <span class="badge">'.$channels['xki'].'</span>' => admin_url('/channel/xki'),
+          '已审核编单 <span class="badge">'.$audit['xki'].'</span>' => admin_url('/channel/xki'),
+        ]
+      ];
+
+      return view('admin.dashboard.statistic', compact('channels'));
     }
 
-    public static function title() {
+    public static function links() {
         $links = [
             '物料管理' => admin_url('/media/material'),
             '黑名单' => admin_url('media/blacklist'),
@@ -98,39 +130,8 @@ HTML;
             'Excel' => admin_url('export/excel')
         ];
 
-        $templates = Statistic::countTemplate();
-        $channels = Statistic::countChannelXml();
-        $audit = Statistic::countAudit();
-        
-        foreach(['xkv','xkc','xki'] as $k) {
-          if(!array_key_exists($k, $channels)) $channels[$k] = 0;
-          if(!array_key_exists($k, $audit)) $audit[$k] = 0;
-          if(!array_key_exists($k, $templates)) $templates[$k] = 0;
-        }
-
-        $channels =[
-          '频道 V China' => [
-            '节目库内容数量 <span class="badge">'.Statistic::countPrograms().'</span>' => admin_url('/media/programs'),
-            '模版库数量 <span class="badge">'.$templates['xkv'].'</span>' => admin_url('/template/xkv'),
-            '编单数量 <span class="badge">'.$channels['xkv'].'</span>' => admin_url('/channel/xkv'),
-            '已审核编单 <span class="badge">'.$audit['xkv'].'</span>' => admin_url('/channel/xkv'),
-          ],
-          '频道 星空中国' => [
-            '节目库内容数量 <span class="badge">'.Statistic::countRecords().'</span>' => admin_url('/media/records'),
-            '模版库数量 <span class="badge">'.$templates['xkc'].'</span>' => admin_url('/template/xkc'),
-            '编单数量 <span class="badge">'.$channels['xkc'].'</span>' => admin_url('/channel/xkc'),
-            '已审核编单 <span class="badge">'.$audit['xkc'].'</span>' => admin_url('/channel/xkc'),
-          ],
-          '频道 星空国际' => [
-            '节目库内容数量 <span class="badge">'.Statistic::countRecord2().'</span>' => admin_url('/media/record2'),
-            '模版库数量 <span class="badge">'.$templates['xki'].'</span>' => admin_url('/template/xki'),
-            '编单数量 <span class="badge">'.$channels['xki'].'</span>' => admin_url('/channel/xki'),
-            '已审核编单 <span class="badge">'.$audit['xki'].'</span>' => admin_url('/channel/xki'),
-          ]
-        ];
-
         $title = '';
-        return view('admin.dashboard', compact('title', 'links', 'channels'));
+        return view('admin.dashboard.links', compact('title', 'links'));
     }
 
     public static function environment()
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return response()->redirectTo('admin');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
