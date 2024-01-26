96ce8207e0f72aecad9c9c6fa8e5030e6ecab392
goslin <goslin@gmail.com>
2024-01-26 20:32:18 +0800

    generage cmd script

--- app/Console/Commands/test.php
+++ app/Console/Commands/test.php
@@ -39,6 +39,26 @@ class test extends Command
         $group = $this->argument('v') ?? "";
         $day = $this->argument('d') ?? "2024-02-06";
 
+
+        $list = DB::table('material')->where('filepath', 'like', '%已播%')->select('filepath','category')->get();
+
+        foreach($list as $line)
+        {
+            switch($line['category'])
+            {
+                case 'CanXin': $this->info("move '".$line['filepath']."' 'Y:\综艺\'" ); break;
+                case 'drama': $this->info("move '".$line['filepath']."' 'Y:\电视剧\'" ); break;
+                case 'cartoon': $this->info("move '".$line['filepath']."' 'Y:\卡通\'" ); break;
+                case 'Entertainm': $this->info("move '".$line['filepath']."' 'Y:\综艺\'" ); break;
+                case 'movie': $this->info("move '".$line['filepath']."' 'Y:\电影\'" ); break;
+                default: $this->info("move '".$line['filepath']."' 'Y:\宣传片垫片\'" ); break;
+            }
+                
+        }
+
+        return 0;
+
+
         $data = $this->getRawData();
         foreach($data as $line)
         {
