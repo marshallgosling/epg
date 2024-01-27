14cd924201e9e6b48fbf717cd67a7c43ac582fb4
goslin <goslin@gmail.com>
2024-01-27 11:17:41 +0800

    modal bug

--- app/Admin/Actions/Program/ProgramMaterial.php
+++ app/Admin/Actions/Program/ProgramMaterial.php
@@ -13,10 +13,10 @@ class ProgramMaterial implements Renderable
         $m = Material::where('unique_no', Program::find($key)->unique_no)->first();
         if($m) {
         
-            $data = '<tr><td><b>'.__('Name').'</b></td><td colspan="3">'.$m->name.'</td></tr>';
-            $data .= '<tr><td><b>'.__('Unique no').'</b></td><td>'.$m->unique_no.'</td><td><b>'.__('Category').'</b></td><td>'.$m->category.'</td></tr>';
-            $data .= '<tr><td><b>'.__('Filepath').'</b></td><td colspan="3">'.$m->filepath.'</td></tr>';
-            $data .= '<tr><td><b>'.__('Duration').'</b></td><td>'.$m->duration.'</td><td><b>'.__('Frames').'</b></td><td>'.$m->frames.'</td></tr>';
+            $data = '<tr><td><b>'.__('Name').'</b></td><td>'.$m->name.'</td></tr>';
+            $data .= '<tr><td><b>'.__('Unique no').'</b></td><td>'.$m->unique_no.'</td></tr><tr><td><b>'.__('Category').'</b></td><td>'.$m->category.'</td></tr>';
+            $data .= '<tr><td><b>'.__('Filepath').'</b></td><td>'.$m->filepath.'</td></tr>';
+            $data .= '<tr><td><b>'.__('Duration').'</b></td><td>'.$m->duration.'</td></tr><tr><td><b>'.__('Frames').'</b></td><td>'.$m->frames.'</td></tr>';
         }
 
         $html = <<<HTML
