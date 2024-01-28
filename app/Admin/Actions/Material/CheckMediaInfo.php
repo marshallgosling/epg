0ae68b7bc9e8562b3e71aea75d6d6024bb35404d
goslin <goslin@gmail.com>
2024-01-28 21:41:36 +0800

    check media info

--- app/Admin/Actions/Material/CheckMediaInfo.php
+++ app/Admin/Actions/Material/CheckMediaInfo.php
@@ -11,11 +11,14 @@ class CheckMediaInfo implements Renderable
     {
         $m = Material::find($key);
         $data = '<tr><td><h4>没有素材 mxf 格式信息，文件：'.$m->filepath.'</h4></td></tr>';
-        if($m && file_exists($m->filepath)) {
-        
-            $data = '<tr><td><b>'.__('Unique no').'</b></td><td>'.$m->unique_no.'</td><td><b>'.__('Category').'</b></td><td>'.$m->category.'</td></tr>';
-            $data .= '<tr><td><b>'.__('Filepath').'</b></td><td colspan="3">'.$m->filepath.'</td></tr>';
-            $data .= '<tr><td><b>MediaInfo</b></td><td colspan="3"><code>'.MediaInfo::geRawInfo($m).'</code></td></tr>';
+        if($m) {
+            $info = MediaInfo::geRawInfo($m);
+            if($info) {
+                $data = '<tr><td><b>'.__('Unique no').'</b></td><td>'.$m->unique_no.'</td><td><b>'.__('Category').'</b></td><td>'.$m->category.'</td></tr>';
+                $data .= '<tr><td><b>'.__('Filepath').'</b></td><td colspan="3">'.$m->filepath.'</td></tr>';
+                $data .= '<tr><td><b>MediaInfo</b></td><td colspan="3" style="height:400px;overflow:scroll;"><code>'.$info.'</code></td></tr>';
+            }
+            
         }
 
         $html = <<<HTML
