601c227ff6c20ae0c6e46e9add0b809ef9135394
goslin <goslin@gmail.com>
2024-02-23 14:56:19 +0800

    Test

--- app/Tools/Exporter/TableGenerator.php
+++ app/Tools/Exporter/TableGenerator.php
@@ -102,6 +102,7 @@ class TableGenerator
 
     public function collectData($start, $end)
     {
+        echo "$start, $end".PHP_EOL;
         $items = DB::table('epg')->join('channel_program', 'epg.program_id','=','channel_program.id')
                 ->select(['epg.name','epg.program_id','epg.start_at','epg.category','channel_program.schedule_start_at','channel_program.schedule_end_at'])
                 ->where('epg.group_id', $this->group)->where('epg.start_at', '>', $start)->where('epg.end_at','<',$end)
