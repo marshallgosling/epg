fc0fdfd91fb086d102f8fa79c0a8bb7fb04095ad
goslin <goslin@gmail.com>
2024-04-05 16:58:51 +0800

    check xml

--- app/Admin/Actions/Channel/CheckXml.php
+++ app/Admin/Actions/Channel/CheckXml.php
@@ -3,14 +3,59 @@ namespace App\Admin\Actions\Channel;
 
 use App\Models\Channel;
 use App\Models\Material;
+use App\Tools\Exporter\BvtExporter;
 use App\Tools\Exporter\XmlReader;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
 
 class CheckXml implements Renderable
 {
+    
     public function render($key = null)
+    {
+        $ch = Channel::find($key);
+
+        $label = '';
+        if(!$ch) $data = '<tr><td>播出编单不存在</td></tr>';
+        else {
+            if(Storage::disk('xml')->exists($ch->name.'_'.$ch->air_date.'.xml')) 
+            {
+                $data = BvtExporter::collectEPG($ch);
+                BvtExporter::generateData($ch, $data);
+                BvtExporter::$file = false;
+                $xml = BvtExporter::exportXml($ch->name);
+
+                $str = Storage::disk('xml')->get($ch->name.'_'.$ch->air_date.'.xml');
+                if( $str == $xml ) {
+                    $label = '<p>播出编单:'.Channel::GROUPS[$ch->name].' 日期:'.$ch->air_date.' 文件:'.$ch->name.'_'.$ch->air_date.'.xml 检查结果：<span class="label label-success">通过</span></p>';
+                }
+                else {
+                    $label = '<p>播出编单:'.Channel::GROUPS[$ch->name].' 日期:'.$ch->air_date.' 文件:'.$ch->name.'_'.$ch->air_date.'.xml 检查结果：<span class="label label-danger">不通过</span></p>';
+                    $data = '<tr><td>播放编单数据和格非串联单xml文件存在差异，请重新“加锁”';
+                }
+            }
+            else {
+                $label = '<p>播出编单:'.$ch->name.'_'.$ch->air_date.'.xml 文件不存在</p>';
+                $data = '';
+            }
+        }
+
+        $html = <<<HTML
+        {$label}
+        <div class="table-responsive">
+            <table class="table table-striped">
+                {$data}
+            </table>
+        </div>
+HTML;
+        
+        return $html;
+    }
+    
+    
+    public function render2($key = null)
     {
         $ch = Channel::find($key);
         $label = '';
